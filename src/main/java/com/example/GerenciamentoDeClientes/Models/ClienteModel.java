b4e5da62f26741c0b5f1af35ee6dbafb02d6c75e
Heitor Ferrari <heitorferrari9@gmail.com>
2026-03-22 21:45:51 -0300

    Model

--- src/main/java/com/example/GerenciamentoDeClientes/Models/ClienteModel.java
+++ src/main/java/com/example/GerenciamentoDeClientes/Models/ClienteModel.java
@@ -1,4 +1,35 @@
 package com.example.GerenciamentoDeClientes.Models;
 
+import jakarta.persistence.*;
+
+@Entity(name = "Cliente")
+@Table(name = "tb_cliente")
 public class ClienteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Long id;
+
+    @Column(name = "nome_cliente")
+    private String nome;
+
+    @Column(name = "email_cliente")
+    private String email;
+
+    private String telefone;
+
+    public ClienteModel() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
 }
